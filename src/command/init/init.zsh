a96072ad9f9f2f5c25e7bc8fe213a44c4fcfde42
Ryooooooga <eial5q265e5@gmail.com>
2023-02-18 21:59:26 +0900

    feat(init): implement async prompt

--- src/command/init/init.zsh
+++ src/command/init/init.zsh
@@ -1,3 +1,32 @@
+croque::prepare-git() {
+  croque prepare git
+}
+
+croque::prepare-git-async::callback() {
+  __croque_git_info="$3"
+  zle reset-prompt
+}
+
+croque::prepare-git-async() {
+  async_stop_worker croque_async_git_worker
+  async_start_worker croque_async_git_worker -n
+  async_register_callback croque_async_git_worker croque::prepare-git-async::callback
+  async_job croque_async_git_worker croque::prepare-git
+}
+
+croque::prepare() {
+  if (( ${+ASYNC_VERSION} )); then
+    async_init
+    croque::prepare-git-async
+  else
+    __croque_git_info="$(croque::prepare-git)"
+  fi
+}
+
+croque::chpwd() {
+  unset __croque_git_info
+}
+
 croque::preexec() {
   __croque_start="$EPOCHREALTIME"
 }
@@ -8,17 +37,20 @@ croque::precmd() {
   local end="$EPOCHREALTIME"
   __croque_duration="$(($end - ${__croque_start:-$end}))"
   unset __croque_start
+
+  croque::prepare
 }
 
 croque::prompt() {
-  croque prompt --exit-status="$__croque_exit_status" --jobs="$__croque_jobs" --duration="$__croque_duration" --width="$COLUMNS" zsh
+  croque prompt --exit-status="$__croque_exit_status" --jobs="$__croque_jobs" --duration="$__croque_duration" --width="$COLUMNS" --data.git="$__croque_git_info" zsh
 }
 
 croque::rprompt() {
-  croque prompt --right --exit-status="$__croque_exit_status" --jobs="$__croque_jobs" --duration="$__croque_duration" --width="$COLUMNS" zsh
+  croque prompt --right --exit-status="$__croque_exit_status" --jobs="$__croque_jobs" --duration="$__croque_duration" --width="$COLUMNS" --data.git="$__croque_git_info" zsh
 }
 
 autoload -Uz add-zsh-hook
+add-zsh-hook chpwd croque::chpwd
 add-zsh-hook precmd croque::precmd
 add-zsh-hook preexec croque::preexec
 
