6c72c458ee7cdf473878f3cb1d744eb629d1fe72
Ryooooooga <eial5q265e5@gmail.com>
2024-10-05 22:01:22 +0900

    feat(segment): add direnv segment

--- src/segment/direnv/mod.rs
+++ src/segment/direnv/mod.rs
@@ -1,4 +1,7 @@
+use std::process::Command;
+
 use super::{Context, Segment, SegmentBuilder};
+use serde::Deserialize;
 
 #[derive(Debug)]
 pub struct DirenvSegmentBuilder {}
@@ -20,8 +23,67 @@ impl SegmentBuilder for DirenvSegmentBuilder {
         let config = &ctx.config.direnv;
         let icons = &config.icons;
 
-        None
+        let status: Status = load_direnv_status()?;
+        let _ = status.state.found_rc?;
+
+        let loaded_rc = status.state.loaded_rc.as_ref();
+
+        let (icon, style) = match loaded_rc {
+            Some(loaded_rc) if loaded_rc.allowed == DIRENV_ALLOWED => {
+                (&icons.loaded, &config.loaded.style)
+            }
+            Some(loaded_rc) if loaded_rc.allowed == DIRENV_DENIED => {
+                (&icons.disallowed, &config.disallowed.style)
+            }
+            _ => (&icons.found, &config.found.style),
+        };
+
+        let content = config.content.replace("{{.status}}", icon);
+
+        Some(Segment {
+            content,
+            style: style.to_ansi(),
+        })
     }
+}
+
+#[derive(Debug, Deserialize)]
+struct Status {
+    // config: Config,
+    state: State,
+}
+
+#[derive(Debug, Deserialize)]
+struct State {
+    #[serde(rename = "foundRC")]
+    found_rc: Option<RC>,
+    #[serde(rename = "loadedRC")]
+    loaded_rc: Option<RC>,
+}
+
+#[derive(Debug, Deserialize)]
+struct RC {
+    allowed: i32,
+    // path: String,
+}
+
+const DIRENV_ALLOWED: i32 = 0;
+// const DIRENV_NOT_ALLOWED: i32 = 1;
+const DIRENV_DENIED: i32 = 2;
+
+fn load_direnv_status() -> Option<Status> {
+    let output = Command::new("direnv")
+        .args(["status", "--json"])
+        .output()
+        .ok()?;
+
+    if !output.status.success() {
+        return None;
+    }
+
+    let status: Status = serde_json::from_slice(&output.stdout).ok()?;
+
+    Some(status)
 }
 
 #[cfg(test)]
